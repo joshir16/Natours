7d38a6f53eb989f4d446d7dd327924cc7c8c6471
Rishabh Joshi <joshir838@gmail.com>
2021-01-31 18:59:31 +0530

    First commit

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-const closeNavBar = document.getElementById("navigation_nav");
-
-const checkbox = function () {
-  document.getElementById("nav_toggle").checked = false;
-};
-
-closeNavBar.addEventListener("click", checkbox);
